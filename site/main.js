001dd376100a5b5ae5ff29c09947f38669b49264
Rob Ashton <robashton@codeofrob.com>
2013-01-28 18:05:12 +0000

    Changed to use new menu configuration

--- site/main.js
+++ site/main.js
@@ -1,49 +1,19 @@
 var Primo = require('primo')
 var physics = require('primo-physics')
-var Menu = require('primo-menu')
+var menu = require('primo-menu')
 var ui = require('primo-ui')
 
 var TinyDefender = require('./game')
-var engine = Primo.Create('game')
 
-var menu = Menu.define(engine)
+var engine = Primo.Create('game')
 var game = new TinyDefender(engine)
 
-menu.configure()
-  .font('50px sans-serif')
-  .defaultColour('#FFF')
-  .viewport(640, 480)
-  .defineScreen("root", function(screen) {
-    screen
-      .displayText('Tiny Defender', 100, 140, '32px comic-sans', '#555')
-      .addOption('Play', 100, 200, function() {
-         menu.hide()
-         game.start()
-      })
-      .addOption('Instructions', 100, 260, 'instructions')
-  })
-  .defineScreen("instructions", function(screen) {
-    screen
-      .addOption("Back", 100, 260, "root")
-      .displayText("You are defending the world, go you", 50, 50, '16px sans-serif')
-      .displayText("Use the arrow keys to move the defender", 50, 70, '16px sans-serif')
-      .displayText("Use the ctrl key to fire", 50, 90, '16px sans-serif')
-      .displayText("Mind you don't run out of energy though!", 50, 110, '16px sans-serif')
-  })
- .defineScreen('gameover', function(screen) {
-    screen
-     .displayText("Game over", 50, 50, '50px sans-serif', '#F00')
-     .displayText("Thanks for playing", 50, 110, '16px sans-serif', '#FF0')
-     .addOption('Play again', 100, 200, function() {
-       game.start()
-       menu.hide()
-    })
- })
 
 engine.on('init', function() {
   physics.init(engine)
   ui.init(engine, { width: 640, height: 480 })
-  menu.show('root')
+  menu.init(engine, configureMenu)
+  engine.menu.show('root')
 })
 
 game.on('game-over', function() {
@@ -60,3 +30,37 @@ engine.render = function() {
 }
 
 engine.start()
+
+function configureMenu(config) {
+  config
+    .font('50px sans-serif')
+    .defaultColour('#FFF')
+    .viewport(640, 480)
+    .defineScreen("root", function(screen) {
+      screen
+        .displayText('Tiny Defender', 100, 140, '32px comic-sans', '#555')
+        .addOption('Play', 100, 200, function() {
+           menu.hide()
+           game.start()
+        })
+        .addOption('Instructions', 100, 260, 'instructions')
+    })
+    .defineScreen("instructions", function(screen) {
+      screen
+        .addOption("Back", 100, 260, "root")
+        .displayText("You are defending the world, go you", 50, 50, '16px sans-serif')
+        .displayText("Use the arrow keys to move the defender", 50, 70, '16px sans-serif')
+        .displayText("Use the ctrl key to fire", 50, 90, '16px sans-serif')
+        .displayText("Mind you don't run out of energy though!", 50, 110, '16px sans-serif')
+    })
+   .defineScreen('gameover', function(screen) {
+      screen
+       .displayText("Game over", 50, 50, '50px sans-serif', '#F00')
+       .displayText("Thanks for playing", 50, 110, '16px sans-serif', '#FF0')
+       .addOption('Play again', 100, 200, function() {
+         game.start()
+         menu.hide()
+      })
+   })
+ }
+
